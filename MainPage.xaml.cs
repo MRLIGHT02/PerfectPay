1b12b89d9755320ed8726b9308def4c7e39e06fc
Saif Ali <saifali42020@gmail.com>
2025-04-23 16:14:53 +0530

    completed design part

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -3,13 +3,18 @@
 public partial class MainPage : ContentPage
 {
 	
+	public decimal Bill { get; set; }
+	public int Tip { get; set; }
+	public int NoOfPerson { get; set; } = 1;
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
+    private void txtBill_Completed(object sender, EventArgs e)
+        {
 
-	
-}
+        }
+    }
 
